3a598bf3110201012dbe4b0487bed5bc59f98aea
Krekumba <56788369+Krekumba@users.noreply.github.com>
2020-12-12 23:06:03 +0100

    losowy generator terenu lepszy 9 chunków

---
+++ projekt junity/Ebezoga/Assets/LosoweObracanie.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LosoweObracanie : MonoBehaviour
+{
+    int losowansko;
+    void Start()
+    {
+        losowansko = Random.Range(0, 4);
+        switch (losowansko)
+        {
+            case 0:
+            gameObject.transform.rotation = Quaternion.Euler(-90, 0, 0);
+            break;
+            case 1:
+            gameObject.transform.rotation = Quaternion.Euler(-90, 90, 0);
+            break;
+            case 2:
+            gameObject.transform.rotation = Quaternion.Euler(-90, 180, 0);
+            break;
+            case 3:
+            gameObject.transform.rotation = Quaternion.Euler(-90, 270, 0);
+            break;
+
+        }
+    }
+
+
+}
